9097a61e180f929f11aa9c1529746d2b2a6aedc1
Lee-Soyeon <leesoyena@gmail.com>
2020-10-12 19:12:32 +0900

    Initial commit

---
+++ Assets/UI/TestCode01.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCode01 : MonoBehaviour
+{
+    public color_pick myColorInst;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("My color is " + myColorInst.lastcolor.ToString());    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pen.color = myColorClass.lastcolor;
+        //Debug.Log("My color is " + myColorInst.lastcolor.ToString());
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            Transform origin = transform;
+            Vector3 pos = new Vector3(origin.position.x, origin.position.y - 0.5f, origin.position.z + 1);
+            Debug.Log(origin.ToString());
+            GameObject spawnedObject = Instantiate(m_PlacedPrefab, pos, Quaternion.identity);
+            m_SessionOrigin.MakeContentAppearAt(spawnedObject.transform, spawnedObject.transform.position, Quaternion.identity);
+            disablePlanes();
+        }
+    }
+}
---
+++ Assets/UI/color_pick.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class color_pick : MonoBehaviour
+{
+    public GameObject colorObj;
+    Vector3 mpos;
+    //public bool isrun = false;
+    public Color lastcolor;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // if (Input.GetMouseButtonDown(2)) { moonrake(); }
+
+        // print (Input.mousePosition);
+        
+        mpos = Input.mousePosition;
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(ScreenShotAndSpoid());
+        }
+         
+        
+    }
+    IEnumerator ScreenShotAndSpoid()
+    {
+        Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        yield return new WaitForEndOfFrame();
+        tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        tex.Apply();
+
+        Color color = tex.GetPixel((int)mpos.x, (int)mpos.y);
+        //colorObj.GetComponent<Renderer>().material.color = color;
+        lastcolor = color;
+
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            colorObj.GetComponent<Image>().color = color;
+        }
+        
+    }
+    //public void OnSpoteButton()
+    //{
+    //    isrun = true;
+    //}
+    //public void OffSpoteButton()    // 펜 입력할 경우 isrun = false;
+    //{
+    //    isrun = false;
+    //}
+}
